113ba54776932d42c90b0d6130db21742a07967f
Aaron Hababag <hababag.aaron>
2021-09-20 21:45:32 +0800

    #2 Add a footer

---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import Jumbotron from 'react-bootstrap/Jumbotron'
+import Button from 'react-bootstrap/Button'
+
+export default function Footer() {
+    return (
+        <Jumbotron>
+            <h1>This is a footer</h1>
+            
+        </Jumbotron>
+    )
+}
